c2e77fd2911f7586063435516eace05250f6807b
Pentasher <pentasher@gmail.com>
2021-03-01 20:11:33 +0800

    Create register page buttons

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,11 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
+    script: [
+      {
+        src: "https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js",
+      },
+    ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
