cfd07ecd79e4b13fc9f8de971de925663d214dd4
Mohammad Alongan <mohammadalongan21@gmail.com>
2021-03-04 14:16:46 +0800

    Fix conflicts again

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,6 @@
 <template>
-<<<<<<< HEAD
-  <div class="sign-in_login">
-    <v-container>
-      <v-row class="center reseta-login-logo">
-        <div class="text-end" style="text-align: center">
-          <img width="70%" src="~/assets/images/reseta-form-img.png" />
-        </div>
-      </v-row>
-    </v-container>
-    <div class="sign-in_login_desc">
-      <v-row class="center">
-        <h1 class="sign-in_login_title text-white">Reseta<b> &nbsp;QRx</b></h1>
-      </v-row>
-      <v-row class="center">
-        <h3 class="text-center text-white">Your Quickest online Prescription.</h3>
-      </v-row>
-    </div>
-    <div class="sign-in_login_buttons">
-      <v-row class="center">
-        <v-btn depressed color="#B2EBF2" class="sign-btn" width="25%">
-          <NuxtLink to="/register">SIGN UP</NuxtLink>
-        </v-btn>
-      </v-row>
-      <v-row class="center">
-        <v-btn class="ma-2 normal-mg login-btn" outlined color="#E0F7FA" width="25%">
-          <NuxtLink to="/login">LOGIN</NuxtLink>
-        </v-btn>
-      </v-row>
-    </div>
-=======
   <div class="reseta">
     <!-- <DashboardHeader /> -->
->>>>>>> 5a1299a6106561222f57988126d93104ebd314a8
   </div>
 </template>
 
@@ -40,49 +9,7 @@
     
   }
 </script>
-<<<<<<< HEAD
-<style scoped>
-.reseta-login-logo {
-  position: relative;
-  top: -145px;
-}
-.sign-in_login_desc {
-  position: relative;
-  top: -90px;
-}
-.sign-in_login_buttons {
-  position: relative;
-  top: -20px;
-}
-a {
-  text-decoration: none;
-  color: white !important;
-}
-.sign-in_login_title {
-  text-shadow: 2px 2px #757575;
-}
-.text-end {
-  width: 190px;
-}
-.sign-btn a {
-  color: #129792 !important;
-}
-.sign-in_login {
-  position: absolute;
-  top: 35%;
-  left: 0;
-  right: 0;
-  height: 100%;
-  width: 100%;
-}
-.sign-btn,
-.login-btn {
-  border-radius: 20px 0px;
-}
-</style>
-=======
 
 <style lang="scss" scoped>
 
 </style>
->>>>>>> 5a1299a6106561222f57988126d93104ebd314a8
