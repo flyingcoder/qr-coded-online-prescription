0a3c99ef2b7e78a12313e24ed8f2e28fbfefb799
Pentasher <pentasher@gmail.com>
2021-03-09 02:32:27 +0800

    Create patient page

--- pages/patient/patient.js
+++ pages/patient/patient.js
@@ -1,4 +1,36 @@
 export default {
-  auth: false,
-  layout: 'default',
+  layout: 'dashboard',
+  data() {
+    return {
+      results: [
+        {
+          icon: 'mdi-doctor',
+          name: 'Dr. Neil Laquizon',
+          swiper: '<<< Swipe left to see <<<',
+          content: 'Send Medical Prescription',
+          date: '12:27 AM',
+        },
+        {
+          icon: 'mdi-account',
+          name: 'Maria Dela Cruz',
+          content: 'Doc pwede ko magpareseta ug akoang maintenance na tambal',
+          date: '8:00 AM',
+        },
+        {
+          icon: 'mdi-hospital',
+          name: 'Options Pharmacy',
+          content:
+            'Good morning sir your medications was bought Full by Ms. Maria Dela Cruz. Thank you for choosing Options Pharmacy.',
+          date: '8:05 AM',
+        },
+        {
+          icon: 'mdi-doctor',
+          name: 'Dr. Weng Artes',
+          swiper: '<<< Swipe left to see <<<',
+          content: 'Sent Medical Prescription',
+          date: 'Dec 25, 2020',
+        },
+      ],
+    }
+  },
 }
