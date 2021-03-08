a01dd775cbe85f7ba9e902752d89813d2b40620d
Pentasher <pentasher@gmail.com>
2021-03-09 01:58:42 +0800

    Finish doctor page

--- pages/doctor/index.vue
+++ pages/doctor/index.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="doctor-main-section">
     <v-container>
-      <v-row v-for="result in results" :key="result">
+      <v-row
+        v-for="result in results"
+        :key="result"
+        class="doctor-patient-messages"
+      >
         <v-col cols="2">
           <v-icon class="outlined">
-            <h2>{{ result.icon }}</h2>
+            {{ result.icon }}
           </v-icon>
         </v-col>
-        <v-col cols="8">
-          {{ result.name }}
+        <v-col cols="10">
+          <div class="doctor-patient-title">{{ result.name }}</div>
+          <div class="doctor-date">
+            {{ result.date }}
+          </div>
+
           <div v-if="result.prescribe" class="prescribed-text">
             {{ result.prescribe }}
           </div>
           {{ result.content }}
         </v-col>
-        <v-col cols="2">
-          {{ result.date }}
-        </v-col>
+        <v-divider inset></v-divider>
       </v-row>
     </v-container>
   </div>
