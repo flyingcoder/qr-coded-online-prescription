a9bae457fa11a05619c379579cc42ce9006ad817
Pentasher <pentasher@gmail.com>
2021-03-13 02:33:34 +0800

    Edit wallet page

--- pages/wallet/index.vue
+++ pages/wallet/index.vue
@@ -1,14 +1,50 @@
 <template>
   <div id="wallet">
+    <v-row class="justify-center">
+      <div class="wallet-title">
+        <span class="wallet-title-e">e</span> - WALLET
+      </div>
+    </v-row>
+    <v-row>
+      <div class="wallet-title align-baseline d-flex full-width">
+        <div class="wallet-title-main-amout-title">
+          <span class="wallet-title-e">e</span> - WALLET
+        </div>
+        <div class="wallet-title-amount">Amount ₱{{ amount }}</div>
+      </div>
+    </v-row>
     <v-row justify="center">
       <v-expansion-panels accordion>
         <v-expansion-panel v-for="(item, i) in payment_method" :key="i.id">
-          <v-expansion-panel-header>{{ item.title }}</v-expansion-panel-header>
+          <v-expansion-panel-header
+            ><v-icon class="wallet-dropdown-icon">{{ item.icon }}</v-icon>
+            <div class="wallet-accordion-title">
+              {{ item.title }}
+            </div></v-expansion-panel-header
+          >
           <v-expansion-panel-content>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
+            <div class="wallet-main-icon">
+              <v-icon large>{{ item.icon_inner }}</v-icon>
+            </div>
+            <div class="wallet-content full-width">
+              <div
+                class="wallet-main-content full-width"
+                v-html="item.content"
+              ></div>
+
+              <div v-if="item.cancel" class="d-flex justify-center">
+                <v-btn elevation="2" text tile class="margin-right-sm">{{
+                  item.yes
+                }}</v-btn>
+                <v-btn elevation="2" text tile>{{ item.cancel }}</v-btn>
+              </div>
+              <div
+                v-if="item.confirm"
+                class="wallet-confirm-selection text-center-pd"
+              >
+                <v-btn elevation="2" text tile>{{ item.confirm }}</v-btn>
+              </div>
+            </div>
           </v-expansion-panel-content>
         </v-expansion-panel>
       </v-expansion-panels>
