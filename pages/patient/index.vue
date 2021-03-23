dcc12682d6647f589e69fa44146e7bde08a12b83
Pentasher <pentasher@gmail.com>
2021-03-23 21:50:52 +0800

    Edit pages

--- pages/patient/index.vue
+++ pages/patient/index.vue
@@ -1,11 +1,26 @@
 <template>
   <div id="dashboard">
     <div id="dashboard">
-      <NewsFeed
-        v-for="newsfeed in newsfeeds"
-        :key="newsfeed.id"
-        :newsfeed="newsfeed"
-      />
+      <div v-if="no_display">
+        <NewsFeed
+          v-for="newsfeed in newsfeeds"
+          :key="newsfeed.id"
+          :newsfeed="newsfeed"
+        />
+      </div>
+      <div v-if="display" class="find-doctors">
+        <nuxt-link to="/doctors">
+          <v-btn
+            depressed
+            color="#1ac6b6"
+            width="100%"
+            class="btn-radius find-doctors-btn"
+            style="color: white"
+          >
+            Find Doctors
+          </v-btn>
+        </nuxt-link>
+      </div>
     </div>
   </div>
 </template>
--- pages/register/_register.vue
+++ pages/register/_register.vue
@@ -7,7 +7,9 @@
           'register-input-card': $nuxt.$route.path == '/register/pharmacy',
         }"
       >
-        <h1>Register as {{ $route.params.register }}</h1>
+        <h1 style="font-size: 27px; font-weight: 400" class="text-center-pd">
+          Register as {{ $route.params.register }}
+        </h1>
         <v-text-field label="First Name" class="login-input">
           <v-icon slot="prepend" color="#1ac6b6" medium> mdi-account </v-icon>
         </v-text-field>
@@ -16,10 +18,14 @@
             mdi-email-multiple
           </v-icon>
         </v-text-field>
-        <v-text-field label="Password" class="login-input">
+        <v-text-field type="password" label="Password" class="login-input">
           <v-icon slot="prepend" color="#1ac6b6" medium> mdi-lock </v-icon>
         </v-text-field>
-        <v-text-field label="Confirm Password" class="login-input">
+        <v-text-field
+          type="password"
+          label="Confirm Password"
+          class="login-input"
+        >
           <v-icon slot="prepend" color="#1ac6b6" medium> mdi-lock </v-icon>
         </v-text-field>
         <v-text-field
@@ -43,19 +49,19 @@
             mdi-card-account-details
           </v-icon>
         </v-text-field>
+        <v-row class="center">
+          <a
+            style="color: white"
+            class="login-main-btn"
+            href="#"
+            :class="{
+              'register-main-btn': $nuxt.$route.path == '/register/pharmacy',
+              'patient-register-btn': $nuxt.$route.path == '/register/patient',
+            }"
+            >REGISTER</a
+          >
+        </v-row>
       </div>
-      <v-row class="center">
-        <a
-          style="color: white"
-          class="login-main-btn"
-          href="#"
-          :class="{
-            'register-main-btn': $nuxt.$route.path == '/register/pharmacy',
-            'patient-register-btn': $nuxt.$route.path == '/register/patient',
-          }"
-          >REGISTER</a
-        >
-      </v-row>
     </v-form>
   </div>
 </template>
