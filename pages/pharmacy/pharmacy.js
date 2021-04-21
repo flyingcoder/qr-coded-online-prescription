9e1102e771c51ec07eda55930645a9f1a5614464
Pentasher <pentasher@gmail.com>
2021-04-22 02:33:32 +0800

    Edit papge

--- pages/pharmacy/pharmacy.js
+++ pages/pharmacy/pharmacy.js
@@ -8,11 +8,27 @@ export default {
       doctors: '',
       pharmacy_medicine: [
         {
-          avatar: '~/assets/images/colchine.png',
+          avatar: '/_nuxt/assets/images/colchine.png',
           name: 'Paracetamol',
           stocks: '200',
           price: '6',
           grams: '0.6mg',
+          brand: 'Dymadon',
+        },
+        {
+          avatar: '/_nuxt/assets/images/colchine.png',
+          name: 'Colchine',
+          stocks: '800',
+          price: '7',
+          grams: '0.6mg',
+          brand: 'Dymadon',
+        },
+        {
+          avatar: '/_nuxt/assets/images/colchine.png',
+          name: 'Biogesic',
+          stocks: '500',
+          price: '8',
+          grams: '0.76mg',
           brand: 'Dymadon',
         },
       ],
