f779515ce67a632d35be30effaab7a0e4edde9ec
Francis Canayon <canayonfrancis@gmail.com>
2023-01-13 16:08:28 +0800

    Update corrections

--- pages/update-password/index.vue
+++ pages/update-password/index.vue
@@ -14,56 +14,82 @@
         </v-btn>
       </template>
     </v-snackbar>
-    <div class="page-title">Update Password</div>
-
-    <div class="back-button" @click="exitprofile">
-      <v-icon color="darken-2"> mdi-close </v-icon>
+    <div class="settings-header">
+      <div class="back-btn align-self-center">
+        <v-icon color="white" @click="exitprofile"> mdi-chevron-left </v-icon>
+      </div>
+      <div class="header-title align-self-center">Settings</div>
     </div>
-    <div class="edit-profile-inputs">
-      <div id="password" class="sm-side-padding">
-        <v-text-field
-          v-model="user_info.password"
-          label="New Password"
-          type="password"
-          outlined
-          dense
-        ></v-text-field>
+    <div class="update-pass__inputs">
+      <div class="user-image">
+        <img
+          v-if="
+            $auth.user.avatar == 'avatar.png' ||
+            $auth.user.avatar ==
+              'https://api.resetaqrx.com/storage/users/user-default.png'
+          "
+          :src="require(`~/assets/images/avatar.png`)"
+          :alt="$auth.user.fname"
+          width="90px"
+          height="90px"
+        />
+        <img
+          v-else
+          :src="$auth.user.avatar"
+          :alt="$auth.user.fname"
+          width="90px"
+          height="90px"
+        />
       </div>
-      <div class="sm-side-padding">
-        <v-text-field
-          v-model="user_info.password_confirmed"
-          label="Confirm Password"
-          type="password"
-          outlined
-          dense
-        ></v-text-field>
-      </div>
-    </div>
+      <div class="d-flex justify-center settings-title">Options</div>
+      <div class="page-title">Update Password</div>
 
-    <div class="page-title">Update Email</div>
-    <div class="edit-profile-inputs">
-      <div id="password" class="sm-side-padding">
-        <v-text-field
-          v-model="user_info.email"
-          label="New Email"
-          autocomplete="off"
-          type="email"
-          value=""
-          :rules="emailRules"
-          outlined
-          dense
-        ></v-text-field>
+      <div class="edit-profile-inputs">
+        <div id="password" class="sm-side-padding">
+          <v-text-field
+            v-model="user_info.password"
+            label="New Password"
+            type="password"
+            outlined
+            dense
+          ></v-text-field>
+        </div>
+        <div class="sm-side-padding">
+          <v-text-field
+            v-model="user_info.password_confirmed"
+            label="Confirm Password"
+            type="password"
+            outlined
+            dense
+          ></v-text-field>
+        </div>
       </div>
-      <div class="sm-side-padding">
-        <v-text-field
-          v-model="user_info.email_confirmed"
-          label="Confirm Email"
-          type="email"
-          :rules="emailRules"
-          value=""
-          outlined
-          dense
-        ></v-text-field>
+
+      <div class="page-title">Update Email</div>
+      <div class="edit-profile-inputs">
+        <div id="password" class="sm-side-padding">
+          <v-text-field
+            v-model="user_info.email"
+            label="New Email"
+            autocomplete="off"
+            type="email"
+            value=""
+            :rules="emailRules"
+            outlined
+            dense
+          ></v-text-field>
+        </div>
+        <div class="sm-side-padding">
+          <v-text-field
+            v-model="user_info.email_confirmed"
+            label="Confirm Email"
+            type="email"
+            :rules="emailRules"
+            value=""
+            outlined
+            dense
+          ></v-text-field>
+        </div>
       </div>
     </div>
     <div class="d-flex justify-center">
